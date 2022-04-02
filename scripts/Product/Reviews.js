da8cd4adcbcbf333099b246e3605ff109b22da5e
DogwishX <zefiru@protonmail.com>
2022-04-02 14:38:48 +0100

    Add firebase config file

--- scripts/Product/Reviews.js
+++ scripts/Product/Reviews.js
@@ -1,4 +1,5 @@
 import { currentProductObj, currentProductRating } from "./Product.js";
+import { createStars } from "./Stars.js";
 
 const reviewHeaderCount = document.querySelector(".product__review-count");
 const reviewOverviewRating = document.querySelector(
@@ -10,3 +11,23 @@ const reviewsCountStr = `${currentProductObj.reviews.length} Reviews`;
 reviewHeaderCount.innerText = `(${currentProductObj.reviews.length})`;
 reviewOverviewRating.innerText = currentProductRating;
 reviewsCountNodeList.forEach((div) => (div.innerText = reviewsCountStr));
+
+// Individual reviews rendering
+const reviewsContainer = document.querySelector(".reviews__container");
+const reviewTemplate = document.querySelector(".reviews__template");
+
+currentProductObj.reviews.forEach(({ rating, title, text, author, date }) => {
+  const newReview = reviewTemplate.content.cloneNode(true);
+  const newReviewRating = newReview.querySelector(".reviews__stars");
+  const newReviewTitle = newReview.querySelector(".reviews__title");
+  const newReviewText = newReview.querySelector(".reviews__text");
+  const newReviewAuthor = newReview.querySelector(".reviews__author");
+  const newReviewDate = newReview.querySelector(".reviews__date");
+
+  createStars(rating, newReviewRating);
+  newReviewTitle.innerText = title;
+  newReviewText.innerText = text;
+  newReviewAuthor.innerText = author;
+  newReviewDate.innerText = date;
+  reviewsContainer.append(newReview);
+});
