f80d10ac69208abae32dcfafce23e9abecef602e
DogwishX <zefiru@protonmail.com>
2022-04-04 10:25:21 +0100

    Add styling and function to activate button on size selection

--- scripts/Product/SizePicker.js
+++ scripts/Product/SizePicker.js
@@ -1,7 +1,22 @@
-const sizeNodeList = document.querySelectorAll(".size");
-sizeNodeList.forEach((size) => size.addEventListener("click", selectSize));
+import { $, $$, nodeListAddEventListeners } from "../utils.js";
+
+const addToCartButton = $(".product__button");
+const sizeNodeList = $$(".size");
+nodeListAddEventListeners(sizeNodeList, "click", selectSize);
 
 function selectSize({ currentTarget }) {
-  sizeNodeList.forEach((size) => size.classList.remove("size--active"));
+  removeSizeStyling();
   currentTarget.classList.add("size--active");
+  activateButton();
+}
+
+function removeSizeStyling() {
+  sizeNodeList.forEach((size) => size.classList.remove("size--active"));
+}
+
+function activateButton() {
+  const productPrice = $(".product__price").innerText;
+  addToCartButton.classList.remove("btn--inactive");
+  addToCartButton.classList.add("btn--active");
+  addToCartButton.innerText = `add to cart - ${productPrice}`;
 }
