1614cd06441556998a5f2a8c5cac07eb28ae318d
DogwishX <zefiru@protonmail.com>
2022-04-07 10:05:39 +0100

    Add tree-pipers page

---
+++ data/products.js
@@ -0,0 +1,102 @@
+const products = [
+  {
+    productId: "tree-runners",
+    productName: "Tree Runners",
+    productType: "shoe",
+    subTitle: "Our lightweight, breathable everyday sneaker.",
+    colors: [
+      {
+        colorId: "mist",
+        colorName: "Mist (White Sole)",
+        colorsHexValues: { top: "#888688", bottom: "#ffffff" },
+        media: [
+          "../Images/products/tree-runners/mist1.avif",
+          "../Images/products/tree-runners/mist2.avif",
+          "../Images/products/tree-runners/mist3.avif",
+          "../Images/products/tree-runners/mist4.avif",
+          "../Images/products/tree-runners/mist5.avif",
+          "../Images/products/tree-runners/mist6.avif",
+          "../Images/products/tree-runners/mist7.avif",
+        ],
+      },
+      {
+        colorId: "buoyant-blue",
+        colorName: "Buoyant Blue (Blizzard Sole)",
+        colorsHexValues: { top: "#409bc9", bottom: "#e0e2dc" },
+        media: [
+          "../Images/products/tree-runners/buoyant-blue1.avif",
+          "../Images/products/tree-runners/buoyant-blue2.avif",
+          "../Images/products/tree-runners/buoyant-blue3.avif",
+          "../Images/products/tree-runners/buoyant-blue4.avif",
+          "../Images/products/tree-runners/buoyant-blue5.avif",
+          "../Images/products/tree-runners/buoyant-blue6.avif",
+          "../Images/products/tree-runners/buoyant-blue7.avif",
+        ],
+      },
+      {
+        colorId: "wasatch",
+        colorName: "Wasatch (White Sole)",
+        colorsHexValues: { top: "#e7bfa4", bottom: "#ffffff" },
+        media: [
+          "../Images/products/tree-runners/wasatch1.avif",
+          "../Images/products/tree-runners/wasatch2.avif",
+          "../Images/products/tree-runners/wasatch3.avif",
+          "../Images/products/tree-runners/wasatch4.avif",
+          "../Images/products/tree-runners/wasatch5.avif",
+          "../Images/products/tree-runners/wasatch6.avif",
+          "../Images/products/tree-runners/wasatch7.avif",
+        ],
+      },
+    ],
+  },
+  {
+    productId: "tree-pipers",
+    productName: "Tree Pipers",
+    productType: "shoe",
+    subTitle:
+      "Our low top sneaker, made with light and breezy eucalyptus tree fiber.",
+    colors: [
+      {
+        colorId: "moonrise",
+        colorName: "Moonrise (White Sole)",
+        colorsHexValues: { top: "#202e3c", bottom: "#ffffff" },
+        media: [
+          "../Images/products/tree-pipers/moonrise1.avif",
+          "../Images/products/tree-pipers/moonrise2.avif",
+          "../Images/products/tree-pipers/moonrise3.avif",
+          "../Images/products/tree-pipers/moonrise4.avif",
+          "../Images/products/tree-pipers/moonrise5.avif",
+          "../Images/products/tree-pipers/moonrise6.avif",
+        ],
+      },
+      {
+        colorId: "lux-purple",
+        colorName: "Lux Purple (Blizzard Sole)",
+        colorsHexValues: { top: "#865c89", bottom: "#e0e2dc" },
+        media: [
+          "../Images/products/tree-pipers/lux-purple1.avif",
+          "../Images/products/tree-pipers/lux-purple2.avif",
+          "../Images/products/tree-pipers/lux-purple3.avif",
+          "../Images/products/tree-pipers/lux-purple4.avif",
+          "../Images/products/tree-pipers/lux-purple5.avif",
+          "../Images/products/tree-pipers/lux-purple6.avif",
+        ],
+      },
+      {
+        colorId: "wasatch",
+        colorName: "Wasatch (White Sole)",
+        colorsHexValues: { top: "#e7bfa4", bottom: "#ffffff" },
+        media: [
+          "../Images/products/tree-pipers/wasatch1.avif",
+          "../Images/products/tree-pipers/wasatch2.avif",
+          "../Images/products/tree-pipers/wasatch3.avif",
+          "../Images/products/tree-pipers/wasatch4.avif",
+          "../Images/products/tree-pipers/wasatch5.avif",
+          "../Images/products/tree-pipers/wasatch6.avif",
+        ],
+      },
+    ],
+  },
+];
+
+export default products;
