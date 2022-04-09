11f83effc84598ba623cec455c0f4725720b6338
DogwishX <zefiru@protonmail.com>
2022-04-09 13:20:04 +0100

    Add function to deactivate menu, when resizing screen

--- scripts/Nav.js
+++ scripts/Nav.js
@@ -5,8 +5,14 @@ const nav = document.querySelector("nav");
 const menuIcon = document.querySelector(".nav__menu-icon");
 
 // Event Listeners
+window.addEventListener("resize", deactivateMenu);
 menuIcon.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   nav.classList.toggle("nav--active");
+}
+
+function deactivateMenu() {
+  if (window.innerWidth <= 768) return;
+  nav.classList.remove("nav--active");
 }
