17de49ecd542b210e4ff016e37fd7ac941a2b1d6
DogwishX <zefiru@protonmail.com>
2022-04-13 14:12:06 +0100

    Figuring out github pages pathing through trial and error

--- scripts/Nav.js
+++ scripts/Nav.js
@@ -1,9 +1,9 @@
 import "./Cart.js";
-import products from "../../data/products.js";
+import { $ } from "./utils.js";
 
 // DOM containers
-const nav = document.querySelector("nav");
-const menuIcon = document.querySelector(".nav__menu-icon");
+const nav = $("nav");
+const menuIcon = $(".nav__menu-icon");
 
 // Event Listeners
 window.addEventListener("resize", deactivateMenu);
